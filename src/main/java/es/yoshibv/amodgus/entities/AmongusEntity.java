338ed6129c84e88e92889f9ae5adad7a04aa8d9a
yoshibv <jomaamga@outlook.es>
2023-04-10 02:09:51 +0200

    some commit

--- src/main/java/es/yoshibv/amodgus/entities/AmongusEntity.java
+++ src/main/java/es/yoshibv/amodgus/entities/AmongusEntity.java
@@ -3,9 +3,13 @@ package es.yoshibv.amodgus.entities;
 import java.util.Random;
 import java.util.UUID;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -15,7 +19,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.NeutralMob;
 import net.minecraft.world.entity.TamableAnimal;
@@ -32,12 +35,14 @@ import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.ForgeMod;
+import net.minecraft.world.scores.Team;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -51,93 +56,71 @@ public class AmongusEntity extends TamableAnimal implements IAnimatable, Neutral
 	@SuppressWarnings("removal")
 	private AnimationFactory factory = new AnimationFactory(this);
 	
+	private static final EntityDataAccessor<Boolean> SITTING = 
+			SynchedEntityData.defineId(AmongusEntity.class, EntityDataSerializers.BOOLEAN);
+	
 	public AmongusEntity(EntityType<? extends TamableAnimal> p_21803_, Level p_21804_) {
 		super(p_21803_, p_21804_);
 		// TODO Apéndice de constructor generado automáticamente
 	}
 	
 	public static AttributeSupplier setAttributes() {
-		return Mob.createMobAttributes()
-				.add(ForgeMod.ENTITY_GRAVITY.get(),1.0f)
-				.add(Attributes.MAX_HEALTH, 20.00)
+		return TamableAnimal.createMobAttributes()
+				.add(Attributes.MAX_HEALTH, 14.00)
 				.add(Attributes.ATTACK_DAMAGE,6.0f)
 				.add(Attributes.ATTACK_SPEED,1.8f)
-				.add(Attributes.MOVEMENT_SPEED, 0.5f).build();
-				
+				.add(Attributes.MOVEMENT_SPEED, 0.3f).build();
 	}
 	
 	@Override
 	protected void registerGoals() {
 	      this.goalSelector.addGoal(1, new FloatGoal(this));
 	      Ingredient eggHatIngredient = Ingredient.of(ForgeRegistries.ITEMS.getValue(new ResourceLocation("amodgus","egghat")));
-	      this.targetSelector.addGoal(4, new OwnerHurtByTargetGoal(this));
-	      this.targetSelector.addGoal(4, new OwnerHurtTargetGoal(this));
-	      this.goalSelector.addGoal(3, new SitWhenOrderedToGoal(this));
+	      this.targetSelector.addGoal(2, new OwnerHurtByTargetGoal(this));
+	      this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
+	      this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
 	      this.goalSelector.addGoal(3, new TemptGoal(this, 1.1D, eggHatIngredient, false));
 	      this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, true));
-	      this.goalSelector.addGoal(6, new FollowOwnerGoal(this, 1.2D, 8.0F, 2.0F, false));
-	      this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 6.0f));
-	      this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-	      this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+	      this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1.2D, 8.0F, 2.0F, false));
+	      this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 6.0f));
+	      this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+	      this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
 	}
 	
-	@Override
-	public InteractionResult mobInteract(Player player, InteractionHand interactionHand) {
-		ItemStack itemstack = player.getItemInHand(interactionHand);
-		if(itemstack.is(ForgeRegistries.ITEMS.getValue(new ResourceLocation("amodgus","egghat")))) {
-			this.customTamingParticles();
-			this.tame(player);
-			return InteractionResult.SUCCESS;
-		}
-		return InteractionResult.FAIL;
-	}
-	
-	private void customTamingParticles() {
-		ParticleOptions particleoptions = ParticleTypes.HEART;
+	@Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
+        return null;
+    }
 		
-		for(int i = 0; i < 7; i++) {
-			double d0 = this.random.nextGaussian() * 0.02D;
-			double d1 = this.random.nextGaussian() * 0.02D;
-			double d2 = this.random.nextGaussian() * 0.02D;
-			this.level.addParticle(particleoptions, this.getRandomX(1.0D), this.getRandomY() + 0.5D, this.getRandomZ(1.0D), d0, d1, d2);
-		}
-	}
-	
 	@SuppressWarnings("removal")
 	private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("walk", true));
             return PlayState.CONTINUE;
         }
+
+        if (this.isSitting()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("sitting", true));
+            return PlayState.CONTINUE;
+        }
+
         event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
         return PlayState.CONTINUE;
     }
-	    
+	
 	@Override
-	public void registerControllers(AnimationData data) {
-		// TODO Apéndice de método generado automáticamente
-		data.addAnimationController(new AnimationController<AmongusEntity>(this, "controller",
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController<AmongusEntity>(this, "controller",
                 0, this::predicate));
-	}
+    }
 
-	@Override
-	public AnimationFactory getFactory() {
-		// TODO Apéndice de método generado automáticamente
-		return factory;
-	}
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
+    }
 
-	@Override
-	public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
-		// TODO Apéndice de método generado automáticamente
-		return null;
-	}
-	
-	public static boolean canSpawn(EntityType<AmongusEntity> entity, LevelAccessor levelAccess, 
-			MobSpawnType spawnType, BlockPos pos, Random random) {
-		return checkAnimalSpawnRules(entity, levelAccess, spawnType, pos, random);
-	}
-	
-	protected void playStepSound(BlockPos pos, BlockState blockIn) {
+    protected void playStepSound(BlockPos pos, BlockState blockIn) {
         this.playSound(SoundEvents.NETHERITE_BLOCK_PLACE, 0.15F, 1.0F);
     }
 
@@ -151,6 +134,102 @@ public class AmongusEntity extends TamableAnimal implements IAnimatable, Neutral
 
     protected float getSoundVolume() {
         return 0.2F;
+    }
+	
+	@Override
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        Item item = itemstack.getItem();
+
+        Item itemForTaming = ForgeRegistries.ITEMS.getValue(new ResourceLocation("amodgus","egghat"));
+
+        if (item == itemForTaming && !isTame()) {
+            if (this.level.isClientSide) {
+                return InteractionResult.CONSUME;
+            } else {
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                if (!ForgeEventFactory.onAnimalTame(this, player)) {
+                    if (!this.level.isClientSide) {
+                        super.tame(player);
+                        this.navigation.recomputePath();
+                        this.setTarget(null);
+                        this.level.broadcastEntityEvent(this, (byte)7);
+                        setSitting(true);
+                    }
+                }
+
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        if(isTame() && !this.level.isClientSide && hand == InteractionHand.MAIN_HAND) {
+            setSitting(!isSitting());
+            return InteractionResult.SUCCESS;
+        }
+
+        if (itemstack.getItem() == itemForTaming) {
+            return InteractionResult.PASS;
+        }
+
+        return super.mobInteract(player, hand);
+    }
+	    
+	public static boolean canSpawn(EntityType<AmongusEntity> entity, LevelAccessor levelAccess, 
+			MobSpawnType spawnType, BlockPos pos, Random random) {
+		return checkAnimalSpawnRules(entity, levelAccess, spawnType, pos, random);
+	}
+    
+	@Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        setSitting(tag.getBoolean("isSitting"));
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        super.addAdditionalSaveData(tag);
+        tag.putBoolean("isSitting", this.isSitting());
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(SITTING, false);
+    }
+
+    public void setSitting(boolean sitting) {
+        this.entityData.set(SITTING, sitting);
+        this.setOrderedToSit(sitting);
+    }
+
+    public boolean isSitting() {
+        return this.entityData.get(SITTING);
+    }
+
+    @Override
+    public Team getTeam() {
+        return super.getTeam();
+    }
+
+    public boolean canBeLeashed(Player player) {
+        return false;
+    }
+
+    @Override
+    public void setTame(boolean tamed) {
+        super.setTame(tamed);
+        if (tamed) {
+            getAttribute(Attributes.MAX_HEALTH).setBaseValue(20.0D);
+            getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(6.0D);
+            getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue((double)0.4f);
+        } else {
+            getAttribute(Attributes.MAX_HEALTH).setBaseValue(14.0D);
+            getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(3.0D);
+            getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue((double)0.3f);
+        }
     }
 
 	@Override
@@ -181,5 +260,9 @@ public class AmongusEntity extends TamableAnimal implements IAnimatable, Neutral
 	public void startPersistentAngerTimer() {
 		// TODO Apéndice de método generado automáticamente
 		
-	}	
+	}
+	
+	
+	
+	
 }
