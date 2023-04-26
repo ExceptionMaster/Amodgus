16f5a09670470ca6f648539702b3668698f3b310
yoshibv <jomaamga@outlook.es>
2023-04-26 14:07:03 +0200

    Painting

--- src/main/java/es/yoshibv/amodgus/Amodgus.java
+++ src/main/java/es/yoshibv/amodgus/Amodgus.java
@@ -4,6 +4,7 @@ import es.yoshibv.amodgus.events.MobsAttrsEvent;
 import es.yoshibv.amodgus.events.MobsRendererEvents;
 import es.yoshibv.amodgus.init.InitItems;
 import es.yoshibv.amodgus.init.MobsInit;
+import es.yoshibv.amodgus.painting.ModPaintings;
 import es.yoshibv.amodgus.sound.ModSounds;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -28,6 +29,7 @@ public class Amodgus {
         MobsInit.register(bus);
         GeckoLib.initialize();
         ModSounds.register(bus);
+        ModPaintings.register(bus);
         
     }
 }
