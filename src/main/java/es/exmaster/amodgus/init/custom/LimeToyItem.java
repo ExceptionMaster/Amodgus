4c2f7fbfb361d8b92fac662b06eba8d5f56583bf
yoshibv <jomaamga@outlook.es>
2023-07-16 08:31:53 +0200

    CAMBIADO USUARIO

---
+++ src/main/java/es/exmaster/amodgus/init/custom/LimeToyItem.java
@@ -0,0 +1,16 @@
+package es.exmaster.amodgus.init.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+
+public class LimeToyItem extends Item{
+	public LimeToyItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        return super.useOn(pContext);
+    }
+}
